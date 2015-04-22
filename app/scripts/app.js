523ddce558673d64672e466cb04c7687213c5242
Nathan Sass <nsass711@gmail.com>
2015-04-21 18:54:11 -0700

    added color change properties

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,6 +20,28 @@
     }
   }
 
+  function byte2Hex(n) {
+    var nybHexString = "0123456789ABCDEF";
+    return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
+  }
+
+  function RGB2Color(r,g,b) {
+    return '#' + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
+  }
+
+  function colorText(str,phase) {
+    if (phase == undefined) phase = 0;
+    center = 128;
+    width = 127;
+    frequency = Math.PI*2/str.length;
+    for (var i = 0; i < str.length; ++i) {
+       red   = Math.sin(frequency*i+2+phase) * width + center;
+       green = Math.sin(frequency*i+0+phase) * width + center;
+       blue  = Math.sin(frequency*i+4+phase) * width + center;
+       return RGB2Color(red,green,blue);
+    }
+  }
+
   
   $('nav').on('click', '.nav-sphere', changePage);
 })(window);
